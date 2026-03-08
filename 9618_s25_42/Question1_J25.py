b4f31d63d8e5ed7eea42f2366bed40588296d9b8
AmmarSAA <s.ammarahmed14@gmail.com>
2026-03-09 02:15:47 +0500

    FIX: Update data files and code across multiple papers

--- 9618_s25_42/Question1_J25.py
+++ 9618_s25_42/Question1_J25.py
@@ -3,25 +3,27 @@
 # TopOfStack is a global variable initialised to –1
 TopOfStack = -1
 # Stack is a global array of strings with all elements initialised to "-1"
-Stack = [-1]*20
+Stack = [-1] * 20
 
 
 # b:
+
 
 # Push() takes a string parameter and attempts to store it on the stack
 def Push(data):
     global Stack, TopOfStack
     # returns –1 if the stack is full
-    if TopOfStack >=20:
+    if TopOfStack >= 20:
         return -1
     #  returns 1 if the parameter is successfully pushed onto the stack
     else:
-        TopOfStack +=1
+        TopOfStack += 1
         Stack[TopOfStack] = data
         return 1
-    
+
 
 # c:
+
 
 # Pop() returns the next item from the stack
 def Pop():
@@ -33,9 +35,64 @@ def Pop():
         returnVal = Stack[TopOfStack]
         TopOfStack -= 1
         return returnVal
-    
+
 
 # d:
 
+
 def ReadData(fileName):
     global Stack, TopOfStack
+    try:
+        file = open(fileName, "r")
+        wholeFile = file.read().splitlines()
+        for line in wholeFile:
+            # print(line)
+            success = Push(line)
+            if success == -1:
+                print("Stack full!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# e:
+
+
+def Calculate():
+    data = 0
+    total = int(Pop())
+    num2 = 0
+    operators = ["+", "-", "*", "/", "^"]
+    while data != -1:
+        data = Pop()
+        if data == -1:
+            break
+
+        if data in operators:
+            operator = data
+        else:
+            num2 = int(data)
+
+            # Actual calcualtion
+
+            if operator == "+":
+                total += num2
+            elif operator == "-":
+                total -= num2
+            elif operator == "*":
+                total *= num2
+            elif operator == "/":
+                total /= num2
+            elif operator == "^":
+                total **= num2
+
+    return total
+
+
+# f.i.
+def main():
+    fileName = input("Please enter file name: ")
+    ReadData(f"F:/Data/CS-P4/9618_s25_42/9618_s25_sf_42/{fileName}")
+    print(Calculate())
+
+
+main()
--- 9618_s25_42/Question2_J25.py
+++ 9618_s25_42/Question2_J25.py
@@ -1,25 +1,75 @@
 # a:
-
 class NewRecord:
     def __init__(self, key, item1, item2):
-       key = self.key
-       item1 = self.item1
-       item2 = self.item2
-       
+        self.key = key
+        self.item1 = item1
+        self.item2 = item2
+
 
 # b:i:
 # Write program code to declare the global arrays HashTable and Spare
-HashTable = [NewRecord]*200
-Spare = [NewRecord]*100
+HashTable = [NewRecord] * 200
+Spare = [NewRecord] * 100
+
 
 # b:ii:
 # procedure Initialise() stores an empty record in each element in HashTable and Spare
 def Initialise():
     global HashTable, Spare
-    HashTable = [NewRecord(-1,-1,-1)] *200
-    Spare = [NewRecord(-1,-1,-1)] *100
-    
+    # HashTable = [NewRecord(-1, -1, -1)] * 200
+    # Spare = [NewRecord(-1, -1, -1)] * 100
+    for i in range(100):
+        Spare[i] = NewRecord(-1, -1, -1)
+    for i in range(200):
+        HashTable[i] = NewRecord(-1, -1, -1)
+
+
+# c:
+def CalculateHash(HashKey):
+    HashValue = HashKey % 200
+    return HashValue
+
+
+# d:
+def InsertIntoHash(Record):
+    global HashTable, Spare
+    # print(NewRecord.key)
+    key = CalculateHash(Record.key)
+    if HashTable[key].key == -1:
+        HashTable[key] = Record
+    else:
+        Spare.append(Record)
+
+
+# e:
+def CreateHashTable():
+    Record = NewRecord(-1, -1, -1)
+    with open("F:\\Data\\CS-P4\\9618_s25_42\\9618_s25_sf_42\\HashData.txt", "r") as f:
+        wholeFile = f.read().splitlines()
+        # print("Key\tItem1\tItem2")
+        for line in wholeFile:
+            rawRecord = line.split(",")
+            key = int(rawRecord[0])
+            item1 = int(rawRecord[1])
+            item2 = int(rawRecord[2])
+
+            Record = NewRecord(key, item1, item2)
+            # print(f"{Record.key}\t{Record.item1}\t{Record.item2}")
+            InsertIntoHash(Record)
+
+
+# f:i:
+def PrintSpare():
+    global Spare
+    if Spare.key != -1:
+        print(Spare.key)
+
+
+# f:ii:
 def main():
-    print(Initialise())
-    
+    Initialise()
+    CreateHashTable()
+    PrintSpare()
+
+
 main()
